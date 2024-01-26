c481409ef91af5aa7773e54bfa8608b93b783ce9
Yevhenii2022 <3starosta3@gmail.com>
2024-01-26 10:42:34 +0200

    add template-parts

---
+++ template-parts/clients.php
@@ -0,0 +1,29 @@
+  <section class="clients">
+    <div class="container container--left">
+      <div class="clients__wrapper">
+        <h2 class="clients__title section__title">Що про нас кажуть клієнти</h2>
+
+        <div class="clients__slider swiper ">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide clients__image">
+              <img src="wp-content/themes/pointer-theme/assets/images/clients/clients-img-1.webp" alt="clients__image">
+            </div>
+            <div class="swiper-slide clients__image">
+              <img src="wp-content/themes/pointer-theme/assets/images/clients/clients-img-2.webp" alt="clients__image">
+            </div>
+            <div class="swiper-slide clients__image">
+              <img src="wp-content/themes/pointer-theme/assets/images/clients/clients-img-3.webp" alt="clients__image">
+            </div>
+            <div class="swiper-slide clients__image">
+              <img src="wp-content/themes/pointer-theme/assets/images/clients/clients-img-4.webp" alt="clients__image">
+            </div>
+            <div class="swiper-slide clients__image">
+              <img src="wp-content/themes/pointer-theme/assets/images/clients/clients-img-2.webp" alt="clients__image">
+            </div>
+          </div>
+        </div>
+        <div class="clients__swiper-pagination"></div>
+
+      </div>
+    </div>
+  </section>
---
+++ template-parts/hero.php
@@ -0,0 +1,17 @@
+  <section class="hero">
+    <div class="container">
+      <div class="hero__wrapper">
+        <h1 class="main__title">Розробка конверсійних сайтів під ключ</h1>
+        <p class="hero__text">
+          COOD — стратегічний партнер по розробці програмного забезпечення
+        </p>
+        <a class="button" href="#">
+          <span>Зв’язатись з нами</span>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 24" fill="none">
+            <circle cx="11.5" cy="12" r="11.5" fill="#368B3C" />
+            <path stroke="#fff" d="M6.838 11.69h9.013M12.432 7.96l3.73 3.73-3.73 3.729" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </section>
